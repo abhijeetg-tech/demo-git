275a46cb7359f4953da4046e857bc908d11cb077
abhijeetg-tech <abhijeetg.mlops@gmail.com>
2026-02-24 21:14:06 +0530

    uploading image display streamlit app

--- streamlit_display_img.py
+++ streamlit_display_img.py
@@ -6,5 +6,23 @@ st.subheader("This app demonstrates basic Streamlit feature to display an image"
 st.write("Let's display an image of a big cat!")
 
 from PIL import Image
-img = Image.open("lion.jpg")
+img = Image.open("lion.avif")
 st.image(img, caption="A big cat", use_column_width=True)
+
+# Load images
+img1 = Image.open("lion.avif")
+img2 = Image.open("lion.avif")
+img3 = Image.open("lion.avif")
+
+# Create 3 columns
+col1, col2, col3 = st.columns(3)
+
+# Display images in each column
+with col1:
+    st.image(img1, caption="Lion", use_container_width=True)
+
+with col2:
+    st.image(img2, caption="Lion", use_container_width=True)
+
+with col3:
+    st.image(img3, caption="Lion", use_container_width=True)
